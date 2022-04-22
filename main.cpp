2d451f2afd9a926da2a732cb74342ffadfd629d9
KishimotoEiji <guren0709oresama@gmail.com>
2022-04-22 13:57:48 +0900

    nNumの値をaDataにコピーし、16進数で1バイトずつ表示する

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 //=================================================
 //
 //オンライン利用技術[main.cpp]
-//Date:2022/04/15
 //Author:KishimotoEiji
 //
 //=================================================
@@ -10,30 +9,96 @@
 //インクルード
 //*******************************
 #include <stdio.h>
+#include <string.h>
+
+//*******************************
+//定数の定義
+//*******************************
+namespace
+{
+const int MAX_DATA = 4;		//データの最大数
+}// namespaceはここまで
+
+//*******************************
+//プロトタイプ宣言
+//*******************************
+namespace
+{
+void ZeroClear(unsigned char* aData);
+void CopyNumber(int nNum, unsigned char* aData);
+void Output(unsigned char* aData);
+void WaitPressEnter(void);
+}// namespaceはここまで
 
 //=================================================
 //メイン関数
 //=================================================
 void main(void)
 {
-	int nNum = 0;	//int型変数
+	int nNum = 0x01020304;			//int型変数
+	unsigned char aData[MAX_DATA];	//配列
 
-	//入力
-	printf("\n 値を入力 > ");
-	scanf("%d", &nNum);
+	//ゼロクリア
+	ZeroClear(&aData[0]);
 
-	unsigned char* pNum = (unsigned char*)&nNum;	//ポインタ
+	//nNumの値をaDataにコピーする
+	CopyNumber(nNum, &aData[0]);
 
 	//出力
+	Output(&aData[0]);
+
+	//Enter入力待ち
+	WaitPressEnter();
+}
+
+namespace
+{
+//=================================================
+//ゼロクリア
+//=================================================
+void ZeroClear(unsigned char* aData)
+{
+	memset(&aData[0], 0, sizeof(aData));
+}
+
+//=================================================
+//値をコピーする
+//=================================================
+void CopyNumber(int nNum, unsigned char* aData)
+{
+	//nNumのポインタ
+	unsigned char* pNum = (unsigned char*)&nNum;
+
+	for (int i = 0; i < sizeof(int); i++)
+	{//nNumの値をaDataにコピーする
+		aData[i] = *(pNum + i);
+	}
+}
+
+//=================================================
+//出力
+//=================================================
+void Output(unsigned char* aData)
+{
+	//aDataのポインタ
+	unsigned char* pData = (unsigned char*)&aData[0];
+
+	//メッセージ
 	printf("\n 《 16進数表示 》\n");
 
 	for (int i = 0; i < sizeof(int); i++)
 	{//16進数で表示する
-		printf("\n [ 0x%.2x ]", *(pNum + i));
+		printf("\n [ 0x%.2x ]", *(pData + i));
 	}
+}
 
-	//Enter入力待ち
+//=================================================
+//Enter入力待ち
+//=================================================
+void WaitPressEnter(void)
+{
 	printf("\n\n Press Enter");
 	rewind(stdin);
 	getchar();
 }
+}// namespaceはここまで

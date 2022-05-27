5d0b5a13868a4538611073854449fb61248e026b
KishimotoEiji <guren0709oresama@gmail.com>
2022-05-27 15:51:16 +0900

    【課題】botの作成…途中デス☆

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //=================================================
 //
-//オンライン利用技術[main.cpp]
+//オンライン利用技術～サーバー側～[main.cpp]
 //Author:KishimotoEiji
 //
 //=================================================
@@ -27,67 +27,75 @@ const int ANSWER_NUMBER = 79;	//正解の値
 //=================================================
 void main(void)
 {
-	/* 2.Winsockの初期化関数を実行する */
+	/* Winsockの初期化関数を実行する */
 
 	WSADATA wsaData;
-	int nErr = WSAStartup(WINSOCK_VERSION, &wsaData);	//WSAStartup関数：winsockの初期化処理
+	int nErr = WSAStartup(WINSOCK_VERSION, &wsaData);	//winsockの初期化処理
 
 	if (nErr != 0)
 	{//初期化に失敗した場合(※エラーメッセージを表示して終了)
 		printf("\n 初期化失敗");
 	}
 
-	/* 3.ソケット作成 */
+	/* ソケット作成 */
 
 	SOCKET sockServer;
-	sockServer = socket(AF_INET, SOCK_STREAM, 0);	//socket関数：ソケットを作成する。接続受付用のソケット作成
+	sockServer = socket(AF_INET, SOCK_STREAM, 0);	//ソケットを作成する。接続受付用のソケット作成
 
 	if (sockServer == INVALID_SOCKET)
 	{//エラーメッセージを表示して終了
 		printf("\n error");
 	}
 
-	/* 4.接続を受け付けるための準備 */
+	/* 接続を受け付けるための準備 */
 
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(22333);	/* ポート番号 */
+	addr.sin_port = htons(22333);
 	addr.sin_addr.S_un.S_addr = INADDR_ANY;
-	bind(sockServer, (struct sockaddr*)&addr, sizeof(addr));	//bind関数：ソケットにポートを割り当て
-	listen(sockServer, 10);		/* 保留中の接続最大数 */		//listen関数：接続受付の準備
+	bind(sockServer, (struct sockaddr*)&addr, sizeof(addr));	//ソケットにポートを割り当て
+	listen(sockServer, 10);										//接続受付の準備
 
-	/* 5.接続を待つ */
+	/* 接続を待つ */
 
 	struct sockaddr_in clientAddr;
 	int nLength = sizeof(clientAddr);
-	SOCKET sock = accept(sockServer, (struct sockaddr*)&clientAddr, &nLength);	//accept関数：接続を受け付ける
+	SOCKET sock = accept(sockServer, (struct sockaddr*)&clientAddr, &nLength);	//接続を受け付ける
 
 	//接続元のIPアドレス・ポート番号取得
 	const char* pClientIP = inet_ntoa(clientAddr.sin_addr);
 	int nClientPort = clientAddr.sin_port;
 
-	/* 6.データの送信 */
+	//質問を受信
+	char aRecvQuestion[MAX_DATA] = {};
+	int nRecvByte = recv(sock, &aRecvQuestion[0], sizeof(aRecvQuestion), 0);	//データを受信する
+
+	if (nRecvByte <= 0)
+	{//接続が切断されたら
+		closesocket(sock);		//ソケットのクローズ処理を行う
+
+		/* ※これ以降、送受信はできない */
+	}
+
+	/*
+		ここで何かしら解答
+	*/
 
 	//変数
-	int nAnswer = ANSWER_NUMBER;
-	char aData[MAX_DATA];
-
-	//エンディアン変換(host to network)
-	nAnswer = htonl(nAnswer);
+	char aAnswer[MAX_DATA] = {};
 
 	//解答を送る
-	memcpy(&aData[0], &nAnswer, sizeof(int));
-	send(sock, &aData[0], sizeof(int), 0);	//send関数：データを送信する
+	send(sock, &aAnswer[0], strlen(&aAnswer[0]) + 1, 0);	//データを送信する
 
-	/* 7.接続を切断する */
+	/* 接続を切断する */
 
 	//クライアントとの接続を閉じる
 	closesocket(sock);
 
 	//接続受付用ソケットを閉じる
-	closesocket(sockServer);		//closesocket関数：ソケットを閉じる
+	closesocket(sockServer);		//ソケットを閉じる
 
-	/* 8.Winsock終了処理 */
+	/* Winsock終了処理 */
 
-	WSACleanup();	//WSACleanup関数：winsockの終了処理
+	WSACleanup();	//winsockの終了処理
 }
